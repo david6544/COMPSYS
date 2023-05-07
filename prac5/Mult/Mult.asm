816f99a8a96c5e8d800474803091ac300730d45c
david6544 <david.maslov@student.adelaide.edu.au>
2023-05-07 20:25:28 +0930

    did 2-2

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    // Store R2
+    @R2
+    D=M
+
+    // Loop and add R1 to itself, R2 Times, until R2 is 0
+(LOOP)
+    // Breaking the loop
+    @R2
+    D=M
+    @END
+    D;JEQ
+
+    //Setting R1
+    @R1
+    D=M
+
+    //Adding onto R0
+    @R0
+    M=M+D
+
+    //Iterate down R2
+    @R2
+    M=M-1
+
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
